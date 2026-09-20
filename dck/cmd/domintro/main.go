5100384325349094d54ce8e41ac72a96de48bca2
Olivier Houte <github@houte.com>
2026-09-20 21:26:15 +0200

    refactor: keep DCK implementation alongside preserved original sources

---
+++ dck/cmd/domintro/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+
+	domintro "go-dom-intro/dck"
+)
+
+func main() {
+	ebiten.SetWindowSize(768, 540)
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	ebiten.SetWindowTitle("Remake of the \"Dom intro\" in Golang + Ebiten")
+	ebiten.SetScreenClearedEveryFrame(false)
+	if err := ebiten.RunGame(newDrawOnUpdateGame(domintro.NewGame())); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ dck_assets.go
@@ -0,0 +1,9 @@
+package domintro
+
+import "embed"
+
+// DCKAssetAssets shares an embedded resource with the optional DCK version.
+func DCKAssetAssets() embed.FS { return assets }
+
+// DCKAssetYmData shares an embedded resource with the optional DCK version.
+func DCKAssetYmData() []byte { return ymData }
